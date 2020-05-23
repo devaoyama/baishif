f22b3d4b76c0a2e2e1efb02c478e8c7007ccb16e
keita <aoyamakeita@aoyamakeidainoMacBook-Pro.local>
2020-05-23 15:10:46 +0900

    make jwt authenticate

---
+++ app/Http/Controllers/Auth/SecurityController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\RegisterRequest;
+use App\User;
+use Illuminate\Auth\AuthManager;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class SecurityController extends Controller
+{
+    private $authManager;
+
+    public function __construct(AuthManager $authManager)
+    {
+        $this->authManager = $authManager;
+    }
+
+    public function register(RegisterRequest $request)
+    {
+        $input = $request->only(['name', 'email', 'password', 'password_confirmation']);
+
+        event(new Registered($user = User::registerUser($input)));
+
+        return response()->json($user);
+    }
+
+    public function login(LoginRequest $request)
+    {
+        $input = $request->only(['email', 'password']);
+
+        $guard = $this->authManager->guard('api');
+        $token = $guard->attempt($input);
+
+        if (!$token) {
+            return new JsonResponse(__('auth.failed'));
+        }
+
+        return new JsonResponse($token);
+    }
+
+    public function logout(Request $request)
+    {
+        auth()->logout();
+        return new JsonResponse('Success logout');
+    }
+}
--- app/User.php
+++ app/User.php
@@ -5,8 +5,10 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Hash;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use Notifiable;
 
@@ -36,4 +38,23 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+
+    public static function registerUser(array $data)
+    {
+        return User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => Hash::make($data['password']),
+        ]);
+    }
 }
